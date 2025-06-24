048fc6bf752935d5200517ae7f1d2759442c5c32
DerKracher <ermilovdaniel12@gmail.com>
2025-06-24 09:12:29 +0200

    addded circulaSeekBar with TemperaturUI

--- app/src/main/java/thermo/schule/thermometer/MainActivity.java
+++ app/src/main/java/thermo/schule/thermometer/MainActivity.java
@@ -1,24 +1,26 @@
 package thermo.schule.thermometer;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.devadvance.circularseekbar.CircularSeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        CircularSeekBar circularSeekBar = findViewById(R.id.circularSeekBar);
+        circularSeekBar.setProgress(13);
+        circularSeekBar.setOnTouchListener((v, event) -> true); // blockiert manuelle Änderung
+
+
     }
 }
